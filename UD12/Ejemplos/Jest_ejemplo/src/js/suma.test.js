9f6b9b1ef33112f27f14b9b9987ca1def477e365
sergarb1 <sergi.profesor@gmail.com>
2021-01-13 12:45:02 +0100

    Pequeño error ejemplo Jest

--- UD12/Ejemplos/Jest_ejemplo/src/js/suma.test.js
+++ UD12/Ejemplos/Jest_ejemplo/src/js/suma.test.js
@@ -1,5 +1,5 @@
 // Importamos suma para poder hacer el test
-const suma = require('./suma');
+import {suma} from "./suma.js"
 
 // Realizamos el test en formato Jest para ver si va bien
 test('sumar 1 + 2 es igual a 3', () => {
