678961f45e675734524472c542d968c10cd0223a
sergarb1 <sergi.profesor@gmail.com>
2021-02-10 16:27:29 +0100

    Añadido ESLint a Webpack en UD15

--- UD15/Ejemplos/Ejemplo06BIS-lista-de-tareas-firebase/src/js/firebase.js
+++ UD15/Ejemplos/Ejemplo06BIS-lista-de-tareas-firebase/src/js/firebase.js
@@ -1,21 +1,26 @@
+/* global firebase */
+/* La línea anterior es para que no de error al no encontrar la variable firebase,
+obtenida a través de un CDN */
+"use strict";
+
 // Recordamos, para usar Firebase, hemos instalado plugin de Firebase y de VueFire
 // para que además sea reactivo con Vue. La orden es "npm install vuefire firebase"
 // O podemos usar los CDN para mejor rendimiento
 
-//import Firebase from 'firebase'
+// import Firebase from 'firebase'
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+}
 
 // Initialize Firebase
-const app=  firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig)
 
 // exportamos variable firestore
 export const db = app.firestore()
--- UD15/Ejemplos/Ejemplo06BIS-lista-de-tareas-firebase/src/js/main.js
+++ UD15/Ejemplos/Ejemplo06BIS-lista-de-tareas-firebase/src/js/main.js
@@ -1,12 +1,17 @@
+/* global Vue */
+/* La línea anterior es para que no de error al no encontrar la variable firebase,
+obtenida a través de un CDN */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "app" }] */
+/* La línea anterior es para que no de error al no usarse la variable app */
 // Importamos la base de datos Firestore de Firebase declarada en "firebase.js"
 import { db } from './firebase.js'
-// Importamos de VueFire el plugin para usar Firestore 
+// Importamos de VueFire el plugin para usar Firestore
 import { firestorePlugin } from 'vuefire'
 
-Vue.use(firestorePlugin);
+Vue.use(firestorePlugin)
 
 // Declaramos la aplicación VUE
-let app = new Vue({
+const app = new Vue({
   // Indicamos el ID del Div que contiene la APP Vue
   el: '#app',
   data: {
@@ -17,47 +22,44 @@ let app = new Vue({
   },
   // Enlazamos la variable definida arriba, "tareas" con una coleccion de Firestore.
   // Esto nos permitira que si se actuzaliza la coleccion Firestore, se nos actualizara la variable automaticamente
-  // Aunque no es bidireccional, para escribir, no basta con escribir en la variable, debemos escribir a Firestore 
+  // Aunque no es bidireccional, para escribir, no basta con escribir en la variable, debemos escribir a Firestore
   firestore: {
     // En Firestore, nuestra coleccion se llama "tareas"
-    tareas: db.collection('tareas'),
+    tareas: db.collection('tareas')
   },
   methods: {
     // Metodo para agregar una tarea
     agregarTarea: function () {
-
-
       // Al agregar la tarea, obtenemos el último ID Libre para darselo a la tarea desde Firebase
-   
+
       // Despues, añadimos la tarea en Firestore
       db.collection('tareas').add({
         nombre: this.nuevaTarea.trim(),
         fecha: new Date(),
         finalizada: false
         // Despues de añadir la tarea, la actualizamos añadiendo el id autogenerado por firestore
-      }).then(function(docRef) {
-        //console.log("Document written with ID: ", docRef.id);
+      }).then(function (docRef) {
+        // console.log("Document written with ID: ", docRef.id);
         docRef.update({ idTarea: docRef.id })
       })
 
-
       // Tras agregar, pone nuevaTarea a vacio y marca mensajeTareaAgregada
-      this.nuevaTarea = '';
-      this.mensajeTareaAgregada = true;
+      this.nuevaTarea = ''
+      this.mensajeTareaAgregada = true
     },
     // Actualizamos en Firebase el estado de la tarea finalizada
-    actualizarEstadoTareaFinalizada: function(tarea){
+    actualizarEstadoTareaFinalizada: function (tarea) {
       // Busca el indice de la tarea
-      let indice = this.tareas.indexOf(tarea);
+      const indice = this.tareas.indexOf(tarea)
       // Obtenemos el idTarea de la tarea y actualizamos la propiedad "finalizada"
-      db.collection('tareas').doc(this.tareas[indice].idTarea).update({ finalizada: !this.tareas[indice].finalizada})
+      db.collection('tareas').doc(this.tareas[indice].idTarea).update({ finalizada: !this.tareas[indice].finalizada })
     },
     // Eliminamos una tarea de Firebase
     eliminarTarea: function (tarea) {
       // Busca el indice de la tarea
-      let indice = this.tareas.indexOf(tarea);
+      const indice = this.tareas.indexOf(tarea)
       // Obtenemos el idTarea de la tarea y eliminamos ese documento de la coleccion firestore
-      db.collection('tareas').doc(this.tareas[indice].idTarea).delete();
+      db.collection('tareas').doc(this.tareas[indice].idTarea).delete()
     }
   }
-});
+})
